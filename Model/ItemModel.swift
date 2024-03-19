4fd1a5699d74586fbb38754877599280f9764882
Drew Morton <123219140+dmorto24@users.noreply.github.com>
2024-03-19 11:04:51 -0600

    Add files via upload

---
+++ Model/ItemModel.swift
@@ -0,0 +1,16 @@
+//
+//  ItemModel.swift
+//  SamsHealthHub
+//
+//  Created by Drew Morton on 3/19/24.
+//
+
+import Foundation
+
+struct ItemModel:Identifiable{
+    
+    var id: String
+    var name: String
+    var description: String
+    
+}
