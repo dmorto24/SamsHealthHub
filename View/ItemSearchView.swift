72ca48e1197358eb1b1193e72633da32e4d6d415
Drew Morton <123219140+dmorto24@users.noreply.github.com>
2024-03-19 11:35:11 -0600

    Add files via upload

--- View/ItemSearchView.swift
+++ View/ItemSearchView.swift
@@ -8,15 +8,21 @@ import SwiftUI
 
 struct ItemSearchView: View {
     @ObservedObject var model = ItemViewModel()
+    @State private var searchText = ""
+    
     var body: some View {
-        List (model.list){
-            item in Text(item.name)
+        NavigationView{
+            List (model.filteredItems(for: searchText)){
+                item in Text(item.name)
+            }
+            .searchable(text: $searchText)
         }
     }
     init(){
         model.getItems()
     }
 }
+
 
 #Preview {
     ItemSearchView()
