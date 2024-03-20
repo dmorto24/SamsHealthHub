76986e8e282e7361e7276f4f03ef376f02afca62
Drew Morton <123219140+dmorto24@users.noreply.github.com>
2024-03-20 15:45:31 -0600

    Add files via upload

---
+++ Model/InformationModel.swift
@@ -0,0 +1,22 @@
+//
+//  InformationModel.swift
+//  SamsHealthHub
+//
+//  Created by Drew Morton on 3/20/24.
+//
+
+import Foundation
+
+struct Information:Identifiable{
+    
+    var id: String
+    var iid: String
+    var calories: Int
+    var carbs: Int
+    var cholesterol: Int
+    var fat: Int
+    var protein: Int
+    var sodium: Int
+    
+    
+}
