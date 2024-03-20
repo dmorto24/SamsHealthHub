de89f34b164d4ec607363541955cc2530ed6d246
CClavey <116412808+CClavey@users.noreply.github.com>
2024-03-20 15:30:21 -0500

    Update SideMenuView.swift

--- View/SideMenuView.swift
+++ View/SideMenuView.swift
@@ -10,16 +10,32 @@ import SwiftUI
 struct SideMenuView: View {
     var body: some View {
         VStack(spacing: 65) {
+            NavigationLink(destination: IndexView()) {
+                Text("Home")
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .navigationBarBackButtonHidden(true)
+            }
+            
             NavigationLink(destination: SettingsView()) {
                 Text("Settings")
                     .font(.title)
                     .foregroundColor(.white)
+                    .navigationBarBackButtonHidden(true)
             }
-    
+            
             NavigationLink(destination: GoalEditView()) {
                 Text("Change Goal")
                     .font(.title)
                     .foregroundColor(.white)
+                    .navigationBarBackButtonHidden(true)
+            }
+            
+            NavigationLink(destination: ItemSearchView()) {
+                Text("Find Item")
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .navigationBarBackButtonHidden(true)
             }
             
             Spacer()
