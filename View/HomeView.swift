7c4accd3820a02759696e21f6d907828d1b989f3
CClavey <116412808+CClavey@users.noreply.github.com>
2024-03-22 17:04:27 -0500

    Update HomeView.swift

--- View/HomeView.swift
+++ View/HomeView.swift
@@ -1,29 +1,133 @@
 //
-//  index.swift
+//  HomeView.swift
 //  SamsHealthHub
 //
 //  Created by Cole Clavey on 3/18/24.
 //
 import SwiftUI
+//import UIKit
 
 struct HomeView: View {
     @State private var showMenu: Bool = false
-    @State private var userHasGoals: Bool = false
+    @State private var userHasGoals: Bool = true
+    //HERE ARE THE DUMMIES, I HATE YOU DUMMIES PLEASE KYS AND DIE
+    @State private var caloriesPreference = "No Preference"
+    @State private var carbsPreference = "No Preference"
+    @State private var cholesterolPreference = "No Preference"
+    @State private var fatPreference = "No Preference"
+    @State private var proteinPreference = "No Preference"
+    @State private var sodiumPreference = "No Preference"
+    
+    //Function for checking if userHasGoals, if so set the variable to true
+    func GoalChecker(){
+        if userHasGoals == true{
+            //Include the code to retrieve the preference data here
+            caloriesPreference = "Medium"
+            carbsPreference = "Medium"
+            cholesterolPreference = "Low"
+            fatPreference = "Low"
+            proteinPreference = "High"
+            sodiumPreference = "Low"
+        }
+    }
     
     var body: some View {
-        //if userHasGoals == true{
-            
-        //}
-        //else{
-            
-        //}
-        
-        
-        
         NavigationView {
             ZStack{
                 VStack {
+                    if userHasGoals == true{
+                        RoundedRectangle(cornerRadius: 10)
+                            .foregroundColor(Color.blue.opacity(0.3))
+                            .frame(height: 200)
+                            .padding()
+                            .overlay(
+                                VStack(alignment: .leading, spacing: 10) {
+                                    HStack {
+                                        Text("Calories: \(caloriesPreference)")
+                                        Rectangle()
+                                        .fill(caloriesPreference == "No Preference" ? Color.gray : (caloriesPreference == "High" ? Color.green : (caloriesPreference == "Low" ? Color.red : Color.yellow)))
+                                        .overlay(
+                                            Text(caloriesPreference == "High" ? "+" : (caloriesPreference == "Low" ? "-" : (caloriesPreference == "Medium" ? "=" : "")))
+                                                .foregroundColor(.black)
+                                        )
+                                        .frame(width: 15, height: 20)
+                                        .padding(.leading, 5)
+                                    }
+                                    HStack{
+                                        Text("Carbs: \(carbsPreference)")
+                                        Rectangle()
+                                        .fill(carbsPreference == "No Preference" ? Color.gray : (carbsPreference == "High" ? Color.green : (carbsPreference == "Low" ? Color.red : Color.yellow)))
+                                        .overlay(
+                                            Text(carbsPreference == "High" ? "+" : (carbsPreference == "Low" ? "-" : (carbsPreference == "Medium" ? "=" : "")))
+                                                .foregroundColor(.black)
+                                        )
+                                        .frame(width: 15, height: 20)
+                                        .padding(.leading, 5)
+                                    }
+                                    HStack{
+                                        Text("Cholesterol: \(cholesterolPreference)")
+                                        Rectangle()
+                                        .fill(cholesterolPreference == "No Preference" ? Color.gray : (cholesterolPreference == "High" ? Color.green : (cholesterolPreference == "Low" ? Color.red : Color.yellow)))
+                                        .overlay(
+                                            Text(cholesterolPreference == "High" ? "+" : (cholesterolPreference == "Low" ? "-" : (cholesterolPreference == "Medium" ? "=" : "")))
+                                                .foregroundColor(.black)
+                                        )
+                                        .frame(width: 15, height: 20)
+                                        .padding(.leading, 5)
+                                    }
+                                    HStack{
+                                        Text("Fat: \(fatPreference)")
+                                        Rectangle()
+                                        .fill(fatPreference == "No Preference" ? Color.gray : (fatPreference == "High" ? Color.green : (fatPreference == "Low" ? Color.red : Color.yellow)))
+                                        .overlay(
+                                            Text(fatPreference == "High" ? "+" : (fatPreference == "Low" ? "-" : (fatPreference == "Medium" ? "=" : "")))
+                                                .foregroundColor(.black)
+                                        )
+                                        .frame(width: 15, height: 20)
+                                        .padding(.leading, 5)
+                                    }
+                                    HStack{
+                                        Text("Protein: \(proteinPreference)")
+                                        Rectangle()
+                                        .fill(proteinPreference == "No Preference" ? Color.gray : (proteinPreference == "High" ? Color.green : (proteinPreference == "Low" ? Color.red : Color.yellow)))
+                                        .overlay(
+                                            Text(proteinPreference == "High" ? "+" : (proteinPreference == "Low" ? "-" : (proteinPreference == "Medium" ? "=" : "")))
+                                                .foregroundColor(.black)
+                                        )
+                                        .frame(width: 15, height: 20)
+                                        .padding(.leading, 5)
+                                    }
+                                    HStack{
+                                        Text("Sodium: \(sodiumPreference)")
+                                        Rectangle()
+                                        .fill(sodiumPreference == "No Preference" ? Color.gray : (sodiumPreference == "High" ? Color.green : (sodiumPreference == "Low" ? Color.red : Color.yellow)))
+                                        .overlay(
+                                            Text(sodiumPreference == "High" ? "+" : (sodiumPreference == "Low" ? "-" : (sodiumPreference == "Medium" ? "=" : "")))
+                                                .foregroundColor(.black)
+                                        )
+                                        .frame(width: 15, height: 20)
+                                        .padding(.leading, 5)
+                                    }
+                                }
+                                .padding()
+                            )
+                    }
+                    else {
+                        RoundedRectangle(cornerRadius: 10)
+                            .foregroundColor(Color.blue.opacity(0.3))
+                            .frame(height: 200)
+                            .padding()
+                            .overlay(
+                                VStack(alignment: .leading, spacing: 10) {
+                                    Text("No Goals Created!")
+                                    Text("Use the Button Below to Add a Goal!")
+                                }
+                                .padding()
+                            )
+                        }
+                
                     Spacer()
+                    
                     if userHasGoals == true {
                         NavigationLink(destination: ContentView()) {
                             ButtonView(title: "Goals")
@@ -65,6 +169,10 @@ struct HomeView: View {
                         .foregroundColor(showMenu ? .red : .primary)
                 }
             }
+        }
+        .navigationBarBackButtonHidden(true)
+        .onAppear{
+            GoalChecker()
         }
     }
 }
