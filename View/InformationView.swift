0c420c8e8f95283577e56f36294e727ce968201f
Drew Morton <123219140+dmorto24@users.noreply.github.com>
2024-04-03 10:04:00 -0500

    Update InformationView.swift

--- View/InformationView.swift
+++ View/InformationView.swift
@@ -1,9 +1,16 @@
 //
+
 //  InformationView.swift
+
 //  SamsHealthHub
+
 //
+
 //  Created by Drew Morton on 3/20/24.
+
 //
+
+
 
 import Foundation
 import SwiftUI
@@ -12,29 +19,35 @@ struct ItemDetailView: View {
     let item: ItemModel
     @ObservedObject var model = ItemViewModel()
     @State private var info: Information?
-    
-    var body: some View {
-            VStack(alignment: .leading, spacing: 16) {
-                if let info = info {
-                    Text((item.description))
-                    Text("Calories: \(info.calories)")
-                    Text("Carbs: \(info.carbs)")
-                    Text("Cholesterol: \(info.cholesterol)")
-                    Text("Fat: \(info.fat)")
-                    Text("Protein: \(info.protein)")
-                    Text("Sodium: \(info.sodium)")
 
-                } else {
-                    ProgressView("Loading...")
-                        .onAppear {
-                            model.getItemDetails(item: item) { fetchedInfo in
-                                self.info = fetchedInfo
-                            }
+    var body: some View {
+
+        VStack(alignment: .leading, spacing: 16) {
+
+            if let info = info {
+
+                Text((item.description))
+                Text("Calories: \(info.calories) : \(info.calorieThresh)")
+                    .foregroundColor(info.calorieThresh == "High" ? .green : .red)
+                Text("Carbs: \(info.carbs)g : \(info.carbThresh)")
+                    .foregroundColor(info.carbThresh == "High" ? .green : .red)
+                Text("Cholesterol: \(info.cholesterol)mg : \(info.cholesterolThresh)")
+                    .foregroundColor(info.cholesterolThresh == "High" ? .green : .red)
+                Text("Fat: \(info.fat)g : \(info.fatThresh)")
+                    .foregroundColor(info.fatThresh == "High" ? .green : .red)
+                Text("Protein: \(info.protein)g : \(info.proteinThresh)")
+                    .foregroundColor(info.proteinThresh == "High" ? .green : .red)
+                Text("Sodium: \(info.sodium)mg : \(info.sodiumThresh)")
+                    .foregroundColor(info.sodiumThresh == "High" ? .green : .red)
+            } else {
+                ProgressView("Loading...")
+                    .onAppear {
+                        model.getItemDetails(item: item) { fetchedInfo in
+                            self.info = fetchedInfo
                         }
-                }
+                    }
             }
-            .navigationTitle(item.name)
+        }
+        .navigationTitle(item.name)
     }
 }
-
-
