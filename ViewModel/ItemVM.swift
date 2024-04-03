ce2b2b760d4a5eff3275650c85df88f71a46a423
Drew Morton <123219140+dmorto24@users.noreply.github.com>
2024-04-03 10:06:14 -0500

    Update ItemVM.swift

--- ViewModel/ItemVM.swift
+++ ViewModel/ItemVM.swift
@@ -1,10 +1,3 @@
-//
-//  ItemVM.swift
-//  SamsHealthHub
-//
-//  Created by Drew Morton on 3/19/24.
-//
-
 import Foundation
 import Firebase
 import FirebaseFirestore
@@ -12,66 +5,101 @@ import FirebaseFirestore
 class ItemViewModel: ObservableObject {
     @Published var list = [ItemModel]()
     
-    func getItems(){
-        //reference db
+    func getItems() {
         let db = Firestore.firestore()
-        //read documents
         db.collection("Items").getDocuments { snapshot, error in
-            //check for errors
-            if error == nil{
-                //no errors
-                if let snapshot = snapshot{
-                    DispatchQueue.main.async{
-                        //get all documents and create items
+            if error == nil {
+                if let snapshot = snapshot {
+                    DispatchQueue.main.async {
                         self.list = snapshot.documents.map { document in
-                                                let data = document.data()
-                                                let id = document.documentID
-                                                let name = data["name"] as? String ?? ""
-                                                let description = data["description"] as? String ?? ""
-                                                return ItemModel(id: id, name: name, description: description)
-                                            }
+                            let data = document.data()
+                            let id = document.documentID
+                            let name = data["name"] as? String ?? ""
+                            let description = data["description"] as? String ?? ""
+                            return ItemModel(id: id, name: name, description: description)
+                        }
                     }
                 }
-            }
-            else{
+            } else {
                 print("Error fetching Documents")
             }
         }
     }
+    
     func getItemDetails(item: ItemModel, completion: @escaping (Information?) -> Void) {
-        // Reference to the collection where the specific information about the item is stored
         let db = Firestore.firestore()
         let itemDetailsCollection = db.collection("Information")
-        
-        // Fetch all documents from the Information collection
         itemDetailsCollection.getDocuments { snapshot, error in
             if let error = error {
                 print("Error fetching item details: \(error)")
-                completion(nil) // Call completion handler with nil if there's an error
+                completion(nil)
                 return
             }
             
-            // Filter documents to find the relevant one based on the iid field
             if let documents = snapshot?.documents {
                 for document in documents {
                     let data = document.data()
-                
+
                     if let iidRef = data["iid"] as? DocumentReference {
                         let iid = iidRef.documentID
-                        
-                        // Compare the documentID of the iidRef with the item.id
+
                         if iid == item.id {
-                            // Document found, parse the data and create Information object
                             let calories = data["calories"] as? Int ?? 0
                             let carbs = data["carbs"] as? Int ?? 0
                             let cholesterol = data["cholesterol"] as? Int ?? 0
                             let fat = data["fat"] as? Int ?? 0
                             let protein = data["protein"] as? Int ?? 0
                             let sodium = data["sodium"] as? Int ?? 0
+
+                            let carbCals = carbs * 4
+                            let fatCals = fat * 9
+                            let proteinCals = protein * 4
+                            var calorieResult: String
+                            var carbResult: String
+                            var cholesterolResult: String
+                            var fatResult: String
+                            var proteinResult: String
+                            var sodiumResult: String
                             
-                            let itemInformation = Information(id: document.documentID, iid: iid, calories: calories, carbs: carbs, cholesterol: cholesterol, fat: fat, protein: protein, sodium: sodium)
-                            
-                            completion(itemInformation) // Call completion handler with the fetched itemInformation
+                            if Double(calories) <= 300 {
+                                calorieResult = "Low"
+                            } else {
+                                calorieResult = "High"
+                            }
+
+                            if (Double(carbCals) / Double(calories)) <= 0.65 {
+                                carbResult = "Low"
+                            } else {
+                                carbResult = "High"
+                            }
+
+                            if cholesterol <= 200 {
+                                cholesterolResult = "Low"
+                            } else {
+                                cholesterolResult = "High"
+                            }
+
+                            if (Double(fatCals) / Double(calories)) <= 0.30 {
+                                fatResult = "Low"
+                            } else {
+                                fatResult = "High"
+                            }
+
+                            if (Double(proteinCals) / Double(calories)) <= 0.40 {
+                                proteinResult = "Low"
+                            } else {
+                                proteinResult = "High"
+                            }
+
+                            if sodium <= 350 {
+                                sodiumResult = "Low"
+                            } else {
+                                sodiumResult = "High"
+                            }
+
+                            let itemInformation = Information(id: document.documentID, iid: iid, calories: calories, calorieThresh: calorieResult, carbs: carbs, carbThresh: carbResult, cholesterol: cholesterol, cholesterolThresh: cholesterolResult, fat: fat, fatThresh: fatResult, protein: protein, proteinThresh: proteinResult, sodium: sodium, sodiumThresh: sodiumResult)
+
+                            completion(itemInformation)
                             return
                         }
                     } else {
@@ -79,24 +107,17 @@ class ItemViewModel: ObservableObject {
                     }
                 }
             }
-            
-            // No document found for the specified item
+
             print("No document found for the specified item")
-            completion(nil) // Call completion handler with nil if no document found
+            completion(nil)
         }
     }
 
-
-
-
-
-
-        func filteredItems(for searchText: String) -> [ItemModel] {
-            if searchText.isEmpty {
-                return list // Return all items if search text is empty
-            } else {
-                return list.filter { $0.name.lowercased().contains(searchText.lowercased()) }
-            }
+    func filteredItems(for searchText: String) -> [ItemModel] {
+        if searchText.isEmpty {
+            return list
+        } else {
+            return list.filter { $0.name.lowercased().contains(searchText.lowercased()) }
         }
-        
     }
+}
