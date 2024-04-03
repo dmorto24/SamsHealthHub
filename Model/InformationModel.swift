5e146b26b196790170e172196811f1bdb6d3634b
Drew Morton <123219140+dmorto24@users.noreply.github.com>
2024-04-03 09:59:15 -0500

    Update InformationModel.swift

--- Model/InformationModel.swift
+++ Model/InformationModel.swift
@@ -4,19 +4,21 @@
 //
 //  Created by Drew Morton on 3/20/24.
 //
-
 import Foundation
 
 struct Information:Identifiable{
-    
     var id: String
     var iid: String
     var calories: Int
+    var calorieThresh: String
     var carbs: Int
+    var carbThresh: String
     var cholesterol: Int
+    var cholesterolThresh: String
     var fat: Int
+    var fatThresh: String
     var protein: Int
+    var proteinThresh: String
     var sodium: Int
-    
-    
+    var sodiumThresh: String
 }
